74cbb5020ecb57b805680fb217ed42a8ca8afc01
Grum999 <grum999@grum.fr>
2021-04-21 20:56:59 +0200

    Fix PkTk headers

--- bulinotes/bulinotes/pktk/modules/tokenizer.py
+++ bulinotes/bulinotes/pktk/modules/tokenizer.py
@@ -1,6 +1,8 @@
 #-----------------------------------------------------------------------------
-# Buli Commander
-# Copyright (C) 2020 - Grum999
+# PyKritaToolKit
+# Copyright (C) 2019-2021 - Grum999
+#
+# A toolkit to make pykrita plugin coding easier :-)
 # -----------------------------------------------------------------------------
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,8 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.
 # If not, see https://www.gnu.org/licenses/
-# -----------------------------------------------------------------------------
-# A Krita plugin designed to manage documents
 # -----------------------------------------------------------------------------
 
 
